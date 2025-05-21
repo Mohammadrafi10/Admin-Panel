995660644c797ad769c16d8dcf901418ad3119cc
mohammadrafi10 <Mohammadrafishirzai83@gmail.com>
2025-05-21 14:55:55 +0430

    Add dark mode toggle and enhance styling with Tailwind CSS utilities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDark]);
+
+  const darkMode = () => {
+    setIsDark(!isDark);
+  };
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div
+        className={`container flex justify-center items-center dark:bg-black dark:text-white w-full h-50 ${
+          isDark ? "dark" : "light"
+        }`}
+      >
+        <h1>Hello World</h1>
+      </div>
+      <button
+        className="p-5 bg-blue-500 text-white rounded-md"
+        onClick={darkMode}
+      >
+        Click me
+      </button>
+    </>
   );
 }
 
