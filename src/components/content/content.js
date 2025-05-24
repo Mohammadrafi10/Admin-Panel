e681458c62898b4ab5a380f4a06c1b2625e16dd8
mohammadrafi10 <Mohammadrafishirzai83@gmail.com>
2025-05-24 13:40:09 +0430

    Refactor Home component to include Content component and simplify layout structure.

---
+++ src/components/content/content.js
@@ -0,0 +1,19 @@
+import Navbar from "../../components/navbar/navbar";
+import Sidebar from "../../components/sidebar/sidebar";
+function Content() {
+  return (
+    <div className="flex flex-row-reverse">
+      <Sidebar />
+      <div className="flex-1">
+        <Navbar />
+        <main className="p-4 mt-16 ml-0 mr-16">
+          <h2 className="text-2xl font-semibold dark:text-white">
+            Welcome to Dashboard
+          </h2>
+          {/* {main content is here} */}
+        </main>
+      </div>
+    </div>
+  );
+}
+export default Content;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,19 +1,11 @@
 import Navbar from "../../components/navbar/navbar";
 import Sidebar from "../../components/sidebar/sidebar";
-
+import Content from "../../components/content/content";
 function Home() {
   return (
-    <div className="flex flex-row-reverse">
-      <Sidebar />
-      <div className="flex-1">
-        <Navbar />
-        <main className="p-4 mt-16 ml-0 mr-16">
-          <h2 className="text-2xl font-semibold dark:text-white">
-            Welcome to Dashboard
-          </h2>
-          {/* {main content is here} */}
-        </main>
-      </div>
+    <div>
+      <Navbar />
+      <Content />
     </div>
   );
 }
