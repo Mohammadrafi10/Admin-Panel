94ec67501ffff6963c8f7f3497759df5b9727222
mohammadrafi10 <Mohammadrafishirzai83@gmail.com>
2025-05-24 13:40:28 +0430

    Remove Sidebar import from Home component to streamline code structure.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,4 @@
 import Navbar from "../../components/navbar/navbar";
-import Sidebar from "../../components/sidebar/sidebar";
 import Content from "../../components/content/content";
 function Home() {
   return (
